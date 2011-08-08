2c7bf045543b93bb35cc663636dcc4823756696b
Julian Mendez <mendez@tcs.inf.tu-dresden.de>
2011-08-08 12:47:44 +0000

    inclusion of two new tests

--- uel/uel.plugin/src/test/java/de/tudresden/inf/lat/uel/plugin/processor/ProcessorTest.java
+++ uel/uel.plugin/src/test/java/de/tudresden/inf/lat/uel/plugin/processor/ProcessorTest.java
@@ -17,6 +17,7 @@ import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.Node;
 import org.semanticweb.owlapi.reasoner.OWLReasoner;
 
 import de.tudresden.inf.lat.jcel.owlapi.main.JcelReasoner;
@@ -26,6 +27,8 @@ public class ProcessorTest extends TestCase {
 
 	private static final String ontology01 = "src/test/resources/testOntology-01.krss";
 	private static final String ontology02 = "src/test/resources/testOntology-02.krss";
+	private static final String ontology03 = "src/test/resources/testOntology-03.krss";
+	private static final String ontology04 = "src/test/resources/testOntology-04.krss";
 
 	private OWLOntology createOntology(InputStream input)
 			throws OWLOntologyCreationException {
@@ -44,14 +47,31 @@ public class ProcessorTest extends TestCase {
 	}
 
 	public void test01() throws OWLOntologyCreationException, IOException {
-		tryOntology(ontology01);
+		Set<String> varNames = new HashSet<String>();
+		varNames.add("A1");
+		varNames.add("A4");
+		tryOntology(ontology01, varNames);
 	}
 
 	public void test02() throws OWLOntologyCreationException, IOException {
-		tryOntology(ontology02);
+		Set<String> varNames = new HashSet<String>();
+		varNames.add("A1");
+		varNames.add("A4");
+		tryOntology(ontology02, varNames);
 	}
 
-	private void tryOntology(String ontologyName)
+	public void test03() throws OWLOntologyCreationException, IOException {
+		Set<String> varNames = new HashSet<String>();
+		varNames.add("Z");
+		tryOntology(ontology03, varNames);
+	}
+
+	public void test04() throws OWLOntologyCreationException, IOException {
+		tryOntology(ontology04, new HashSet<String>());
+
+	}
+
+	private void tryOntology(String ontologyName, Set<String> varNames)
 			throws OWLOntologyCreationException, IOException {
 		Map<String, OWLClass> idClassMap = new HashMap<String, OWLClass>();
 		UelProcessor processor = new UelProcessor();
@@ -66,8 +86,9 @@ public class ProcessorTest extends TestCase {
 			}
 		}
 		Set<String> variables = new HashSet<String>();
-		variables.add(idClassMap.get("A1").toStringID());
-		variables.add(idClassMap.get("A4").toStringID());
+		for (String var : varNames) {
+			variables.add(idClassMap.get(var).toStringID());
+		}
 		processor.addAll(variables);
 
 		Set<String> input = new HashSet<String>();
@@ -86,8 +107,10 @@ public class ProcessorTest extends TestCase {
 		for (String unifier : unifiers) {
 			String extendedOntology = goalStr + unifier;
 			OWLReasoner reasoner = createReasoner(extendedOntology);
-			assertTrue(reasoner.getEquivalentClasses(idClassMap.get("C"))
-					.contains(idClassMap.get("D")));
+			Node<OWLClass> node = reasoner.getEquivalentClasses(idClassMap
+					.get("C"));
+			OWLClass elem = idClassMap.get("D");
+			assertTrue(node.contains(elem));
 		}
 	}
 
