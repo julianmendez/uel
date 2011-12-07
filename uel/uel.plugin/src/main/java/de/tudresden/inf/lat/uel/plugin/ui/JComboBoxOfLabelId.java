c2c9cb7a54a10ebaaf9d6011ad15713adf032922
Julian Mendez <mendez@tcs.inf.tu-dresden.de>
2011-12-07 15:49:14 +0000

    improvement of combo box

--- uel/uel.plugin/src/main/java/de/tudresden/inf/lat/uel/plugin/ui/JComboBoxOfLabelId.java
+++ uel/uel.plugin/src/main/java/de/tudresden/inf/lat/uel/plugin/ui/JComboBoxOfLabelId.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 
 /**
@@ -16,7 +17,6 @@ public class JComboBoxOfLabelId extends JComboBox {
 	private static final long serialVersionUID = -1589168297784841281L;
 
 	private List<LabelId> list = new ArrayList<LabelId>();
-	private boolean processing = false;
 
 	public JComboBoxOfLabelId() {
 		super();
@@ -26,12 +26,10 @@ public class JComboBoxOfLabelId extends JComboBox {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		if (!this.processing) {
-			String lastText = event.getActionCommand();
-			if (!lastText.equals(getActionCommand())) {
-				int itemIndex = binarySearch(this.list, lastText);
-				this.setSelectedIndex(itemIndex);
-			}
+		String lastText = event.getActionCommand();
+		if (!lastText.equals(getActionCommand())) {
+			int itemIndex = binarySearch(this.list, lastText);
+			this.setSelectedIndex(itemIndex);
 		}
 	}
 
@@ -45,24 +43,26 @@ public class JComboBoxOfLabelId extends JComboBox {
 		throw new UnsupportedOperationException();
 	}
 
-	private int binarySearch(List<LabelId> list, String origKey) {
-		LabelId key = new LabelId(origKey, origKey);
+	private int binarySearch(List<LabelId> list, String key) {
 		int left = 0;
 		int right = list.size();
 		int mid = left;
 		while (left < right - 1) {
 			mid = (left + right) / 2;
 			LabelId current = list.get(mid);
-			if (current.equals(key)) {
+			if (current.getLabel().equals(key)) {
 				left = mid;
 				right = mid;
-			} else if (current.compareTo(key) < 0) {
+			} else if (current.getLabel().compareTo(key) < 0) {
 				left = mid;
-			} else if (current.compareTo(key) > 0) {
+			} else if (current.getLabel().compareTo(key) > 0) {
 				right = mid;
 			}
 		}
-		return left;
+		int ret = left < list.size() - 1
+				&& list.get(left).getLabel().compareTo(key) < 0 ? left + 1
+				: left;
+		return ret;
 	}
 
 	public LabelId getSelectedElement() {
@@ -70,15 +70,14 @@ public class JComboBoxOfLabelId extends JComboBox {
 	}
 
 	public void setItemList(List<LabelId> origList) {
-		this.processing = true;
 		this.list.clear();
 		this.list.addAll(origList);
 		Collections.sort(this.list);
-		super.removeAllItems();
+		DefaultComboBoxModel model = new DefaultComboBoxModel();
 		for (LabelId label : this.list) {
-			super.addItem(label.getLabel());
+			model.addElement(label.getLabel());
 		}
-		this.processing = false;
+		super.setModel(model);
 	}
 
 }
