e40e9054c67de350b02e3bf1660852e23f104c61
Stefan Borgwardt <stefborg@tcs.inf.tu-dresden.de>
2014-04-08 14:31:03 +0200

    find all solutions with the ASP encoding

--- uel/uel-asp/src/main/java/de/tudresden/inf/lat/uel/asp/solver/ClaspSolver.java
+++ uel/uel-asp/src/main/java/de/tudresden/inf/lat/uel/asp/solver/ClaspSolver.java
@@ -23,8 +23,8 @@ public class ClaspSolver implements AspSolver {
 	private static String HEURISTIC_PROGRAM = "resources/heuristic.lp";
 	private static String FINAL_PROGRAM = "resources/final.lp";
 	private static String GRINGO_COMMAND = "gringo";
-	private static String CLASP_COMMAND = "clasp --project --outf=2";
-	private static String HCLASP_COMMAND = "hclasp --project -e record --outf=2";
+	private static String CLASP_COMMAND = "clasp 0 --project --outf=2";
+	private static String HCLASP_COMMAND = "hclasp 0 --project -e record --outf=2";
 
 	private boolean disequations;
 	private boolean types;
